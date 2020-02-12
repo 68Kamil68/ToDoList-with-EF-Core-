8792f80ac64d899b9165cbba8df8a174c9c0c0e5
Kamil Krakowski <tret1lus@gmail.com>
2020-02-13 00:09:48 +0100

    Add project files.

---
+++ ToDoList/Models/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+    public class Todo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TodoID { get; set; }
+        [Required]
+        public string Value { get; set; }
+        [Required]
+        public bool? Deleted { get; set; } = false;
+        public User User { get; set; }
+    }
+}
---
+++ ToDoList/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        [Required][MaxLength(20)]
+        public string Nickname { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int Level { get; set; } = 1;
+        [Required]
+        public int XP { get; set; } = 0;
+        public ICollection<Todo> Todos { get; set; }
+    }
+}
